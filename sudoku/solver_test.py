7e7febd5e9de27acce4a97dc5a23e9a763f59960
magicmage6 <zheng_yu@hotmail.com>
2019-08-12 00:09:04 -0700

    iterate fast solver and update full solver tests

--- sudoku/solver_test.py
+++ sudoku/solver_test.py
@@ -16,23 +16,39 @@ def read_data_file(file_name):
         expected_solutions.append(lines[i+10])
   return sudoku, sorted(expected_solutions)
 
+def compare_solutions(file_name, solutions, expected_solutions):
+  sorted_solutions = sorted(['{},{},{}'.format(i, j, c) for i, j, c in solutions])
+  if sorted_solutions != expected_solutions:
+    print('Mismtach found for {}.'.format(file_name))
+    print('Expected solutions: {}'.format(expected_solutions))
+    print('Actual solutions: {}'.format(sorted_solutions))
+    raise RuntimeError('Testing failed.')
+
 def test_fast_solver():
   path = 'test_data/fast_solver'
   for file_name in os.listdir(path):
     full_name = os.path.join(path, file_name)
     sudoku, expected_solutions = read_data_file(full_name)
     solver = sudoku_solver.SudokuSolver(sudoku)
-    solutions = sorted(['{},{},{}'.format(i, j, c) for i, j, c in solver.solve(fast_solve=True)])
-    if solutions != expected_solutions:
-      print('Mismtach found for {}.'.format(full_name))
-      print('Expected solutions: {}'.format(expected_solutions))
-      print('Actual solutions: {}'.format(solutions))
-      raise RuntimeError('Testing failed.')
+    solutions = solver.solve(fast_solve=True)
+    compare_solutions(full_name, solutions, expected_solutions)
   print('Fast solver tests passed.')
+
+
+def test_full_solver():
+  path = 'test_data/full_solver'
+  for file_name in os.listdir(path):
+    full_name = os.path.join(path, file_name)
+    sudoku, expected_solutions = read_data_file(full_name)
+    solver = sudoku_solver.SudokuSolver(sudoku)
+    solutions = solver.solve()
+    compare_solutions(full_name, solutions, expected_solutions)
+  print('Full solver tests passed.')
 
 
 def main():
   test_fast_solver()
+  test_full_solver()
   print('Tests passed.')
 
 
--- sudoku/sudoku_solver.py
+++ sudoku/sudoku_solver.py
@@ -1,4 +1,5 @@
 import collections
+import copy
 
 # A region is a row, column, or a box where each number 1-9 will appear once and only once.
 # A region represent a row.
@@ -17,7 +18,9 @@ class SudokuSolver:
     self._location_groups = [{} for i in range(10)]
     # A dictionary mapping the possible locations of a number in a region.
     self._possible_locations = {}
-    # A set of unique locations for a number in a region.
+    # A dictionary of unique locations for a number in a region.
+    # The key is a tupe of the region type, region number, the value (number), and the value
+    # is the only possible location.
     self._unique_locations = {}
 
   def _remove_possible_values(self, row, col, value):
@@ -27,8 +30,26 @@ class SudokuSolver:
       if (row, col) in self._location_groups[orig_len]:
         self._location_groups[orig_len].remove((row, col))
         self._location_groups[orig_len - 1].add((row, col))
+      keys = [(_ROW_REGION, row, value), (_COLUMN_REGION, col, value), (_BOX_REGION, int (row / 3) * 3 + int (col / 3), value)]
+      for key in keys:
+        # print('removing key ', key)
+        if key in self._possible_locations:
+          locations = self._possible_locations[key]
+          if (row, col) in locations:
+            locations.remove((row, col))
+            if len(locations) == 1:
+              for l in locations:
+                self._unique_locations[key] = l
 
   def _update_possible_values(self, row, col, value):
+    possible_values = self._possible_values[row][col]
+    group = self._location_groups[len(possible_values)]
+    if (row, col) in group:
+      group.remove((row, col))
+    for c in copy.copy(possible_values):
+      if c != value:
+        self._remove_possible_values(row, col, c)
+    self._possible_values[row][col] = {}
     for i in range(9):
       if i != row and self._sudoku.get(i, col) == ' ':
         self._remove_possible_values(i, col, value)
@@ -41,6 +62,16 @@ class SudokuSolver:
       for j in range(left, left + 3):
         if (i != row or j != col) and self._sudoku.get(i, j) == ' ':
           self._remove_possible_values(i, j, value)
+    keys = [(_ROW_REGION, row, value), (_COLUMN_REGION, col, value), (_BOX_REGION, int (row / 3) * 3 + int (col / 3), value)]
+    for key in keys:
+      # print('key', key)
+      if key in self._possible_locations:
+        # print('delete')
+        del self._possible_locations[key]
+      if key in self._unique_locations:
+        # print('delete unique')
+        del self._unique_locations[key]
+
 
   def _initialize_possible_values(self):
     for i in range(9):
@@ -81,11 +112,13 @@ class SudokuSolver:
           self._unique_locations[key] = c
 
   def _fast_solve(self):
+    """Sovling it with fast approaches."""
     solutions = []
     solution_set = set()
     # for i in range(9):
       # for j in range(9):
         # print(i, j, self._possible_values[i][j])
+    # Fill in the locations where only one value is possible.
     unique_group = self._location_groups[1]
     if unique_group:
       for i, j in unique_group:
@@ -102,6 +135,7 @@ class SudokuSolver:
     # for key, value in self._unique_locations.items():
       # print(key, value)
 
+    # Fill in the numbers in a region where only one location is possible.
     for key, value in self._unique_locations.items():
       _, _, c = key
       row, col = value
@@ -116,7 +150,15 @@ class SudokuSolver:
     return solutions
   
   def _recursive_solve(self):
-    return self._fast_solve()
+    solutions = []
+    for _ in range(81):
+      fast_solutions = self._fast_solve()
+      # print(fast_solutions)
+      if not fast_solutions:
+        break
+      else:
+        solutions.extend(fast_solutions)
+    return solutions
 
   def solve(self, fast_solve = False):
     """Solve a sudoku."""
