0422725109d72ac316d534aa33a5cc54aabf45e3
magicmage6 <zheng_yu@hotmail.com>
2019-08-13 21:38:25 -0700

    initialize data

--- python_sudoku/sudoku_solver.py
+++ python_sudoku/sudoku_solver.py
@@ -133,6 +133,11 @@ class SudokuSolver:
       if len(possible_locations) == 1:
         for location in possible_locations:
           self._unique_locations[key] = location
+  
+  def _initialize_data(self):
+    """Initialize possible values and possible locations."""
+    self._initialize_possible_values()
+    self._initialize_possible_locations()
 
   def _fast_solve(self):
     """Solving a sudoku with fast approaches.
@@ -231,8 +236,8 @@ class SudokuSolver:
           if try_solutions is None:
             # Fail to get valid solution, revert the try.
             self._sudoku.set(row, col, ' ')
-            self._initialize_possible_values()
-            self._initialize_possible_locations()
+            # We can incrementally update, but just reinitialize it seems to be fast enough.
+            self._initialize_data()
           else:
             # Get a valid solution.
             solution.append((row, col, value))
@@ -242,8 +247,8 @@ class SudokuSolver:
           # Can't find valid solution. Revert fast solutions.
           for row, col, _ in solution:
             self._sudoku.set(row, col, ' ')
-          self._initialize_possible_values()
-          self._initialize_possible_locations()
+          # We can incrementally update, but just reinitialize it seems to be fast enough.
+          self._initialize_data()
           return None
         break
       break
@@ -298,8 +303,7 @@ class SudokuSolver:
     """
     if simple:
       return self._simple_solve()
-    self._initialize_possible_values()
-    self._initialize_possible_locations()
+    self._initialize_data()
     if fast:
       return self._fast_solve()
     return self._full_solve()
