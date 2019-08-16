cf213e17daed1fce5a9adef6f5d48f27e05caf7b
magicmage6 <zheng_yu@hotmail.com>
2019-08-15 21:55:57 -0700

    change sudoku generator

--- python_sudoku/generator_test.py
+++ python_sudoku/generator_test.py
@@ -2,29 +2,19 @@ import sudoku_generator
 import sudoku_solver
 
 
-def test_generator(level, nr_tests):
-  for _ in range(nr_tests):
-    generator = sudoku_generator.SudokuGenerator()
-    sudoku = generator.get_sudoku(level=level)
-    solver = sudoku_solver.SudokuSolver()
-    solution = solver.solve(sudoku)
-    passed = True
-    if not solution:
-      print('Can not solve the generated sudoku.')
-      passed = False
-    expected_nr_spaces = sudoku_generator._NR_SPACES_DICT[level]
-    if len(solution) != expected_nr_spaces:
-      print('Expected nr spaces {}. Actual {}'.format(expected_nr_spaces,
-                                                      len(solution)))
-      passed = False
-    if not passed:
-      raise RuntimeError('Test for {} level failed.'.format(level))
-  print('Tests passed for {} level.'.format(level))
-
+def test_generator(generator, level):
+  sudoku = generator.get_sudoku(level=level)
+  solver = sudoku_solver.SudokuSolver()
+  solution = solver.solve(sudoku)
+  if not solution:
+    print('Can not solve the generated sudoku.')
+    raise RuntimeError('Test for {} level failed.'.format(level))
 
 def test_generators():
-  test_generator('EASY', 100)
-  test_generator('MEDIUM', 50)
-  test_generator('HARD', 10)
-  test_generator('CHALLENGER', 10)
+  generator = sudoku_generator.SudokuGenerator()
+  for _ in range(10):
+    test_generator(generator, 'EASY')
+    test_generator(generator, 'MEDIUM')
+    test_generator(generator, 'HARD')
+    test_generator(generator, 'CHALLENGER')
   print('Tests passed.')
--- python_sudoku/sudoku_generator.py
+++ python_sudoku/sudoku_generator.py
@@ -4,8 +4,6 @@ import random
 import sudoku_data
 import sudoku_solver
 
-_NR_SPACES_DICT = {'EASY': 45, 'MEDIUM': 47, 'HARD': 50, 'CHALLENGER': 52}
-
 
 class SudokuGenerator:
   """Class for sudoku generator."""
@@ -15,6 +13,7 @@ class SudokuGenerator:
     self._solver = sudoku_solver.SudokuSolver()
     self._max_solver = sudoku_solver.SudokuSolver(randomize_type = 'max')
     self._min_solver = sudoku_solver.SudokuSolver(randomize_type = 'min')
+    self._sudoku_map = {'EASY': [], 'MEDIUM': [], 'HARD': [], 'CHALLENGER': []}
 
   def is_partial_solvable(self, sudoku):
     """Whether the sudoku can be solved by partial solver."""
@@ -26,6 +25,28 @@ class SudokuGenerator:
         break
     return clone.is_solved()
 
+  def make_one_solution(self, sudoku, full_sudoku):
+    """Make a sudoku has only one solution."""
+    for _ in range(80):
+      clone1 = sudoku_data.SudokuData()
+      clone1.copy(sudoku)
+      self._max_solver.solve(clone1)
+      clone2 = sudoku_data.SudokuData()
+      clone2.copy(sudoku)
+      self._min_solver.solve(clone2)
+      is_same = True
+      for row in range(9):
+        for col in range(9):
+          if clone1.get(row, col) != clone2.get(row, col):
+            is_same = False
+            sudoku.set(row, col, full_sudoku.get(row, col))
+            break
+        if not is_same:
+          break
+      if is_same:
+        return
+
+      
   def has_only_one_solution(self, sudoku):
     """Whether the sudoku has only one solution."""
     clone1 = sudoku_data.SudokuData()
@@ -42,15 +63,15 @@ class SudokuGenerator:
     Returns:
       A random generated sudoku problem.
     """
-    sudoku = sudoku_data.SudokuData()
     level = level.upper()
-    if level not in _NR_SPACES_DICT:
-      raise ValueError('Level {} is not valid. Valid levels are {}.'.format(
-          level, _NR_SPACES_DICT.keys()))
-    nr_spaces = _NR_SPACES_DICT[level]
-    for _ in range(1000):
+    if level not in {'EASY', 'MEDIUM', 'HARD', 'CHALLENGER'}:
+      raise ValueError('Level {} is not valid.'.format(level))
+    for _ in range(100):
+      nr_spaces = 56
       sudoku = sudoku_data.SudokuData()
       self._solver.solve(sudoku)
+      full_sudoku = sudoku_data.SudokuData()
+      full_sudoku.copy(sudoku)
       nr_removed = 0
       while nr_removed < nr_spaces:
         row = random.randrange(9)
@@ -58,13 +79,26 @@ class SudokuGenerator:
         if sudoku.get(row, col) != ' ':
           sudoku.set(row, col, ' ')
           nr_removed += 1
-      if level == 'EASY' or level == 'MEDIUM':
-        if self.is_partial_solvable(sudoku):
-          break
-      elif level == 'HARD':
-        if self.has_only_one_solution(sudoku):
-          break
+      self.make_one_solution(sudoku, full_sudoku)
+      clone = sudoku_data.SudokuData()
+      clone.copy(sudoku)
+      solution = self._solver.solve(clone)
+      nr_missing = len(solution)
+      curr_level = 'EASY'
+      if not self.is_partial_solvable(sudoku) or nr_missing > 51:
+        curr_level = 'CHALLENGER'
+      elif nr_missing <= 45:
+        curr_level = 'EASY'
+      elif nr_missing <= 48:
+        curr_level = 'MEDIUM'
       else:
-        if self.has_only_one_solution(sudoku) and not self.is_partial_solvable(sudoku):
-          break
+        curr_level = 'HARD'
+      sudoku_list = self._sudoku_map[curr_level]
+      if len(sudoku_list) < 100:
+        sudoku_list.append(sudoku)
+      sudoku_list = self._sudoku_map[level]
+      if sudoku_list:
+        sudoku = sudoku_list[-1]
+        del sudoku_list[-1]
+        break
     return sudoku
