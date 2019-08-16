b0e4c18e3bcb6eb1bba34df0a2fe1deefd6c7401
magicmage6 <zheng_yu@hotmail.com>
2019-08-15 22:03:50 -0700

    modify the test

--- python_sudoku/generator_test.py
+++ python_sudoku/generator_test.py
@@ -12,9 +12,16 @@ def test_generator(generator, level):
 
 def test_generators():
   generator = sudoku_generator.SudokuGenerator()
-  for _ in range(10):
-    test_generator(generator, 'EASY')
+  for _ in range(50):
     test_generator(generator, 'MEDIUM')
+  print('Tests for MEDIUM level passed.')
+  for _ in range(20):
+    test_generator(generator, 'EASY')
+  print('Tests for EASY level passed.')
+  for _ in range(20):
     test_generator(generator, 'HARD')
+  print('Tests for HARD level passed.')
+  for _ in range(10):
     test_generator(generator, 'CHALLENGER')
-  print('Tests passed.')
+  print('Tests for CHALLENGER level passed.')
+  print('All tests passed.')
